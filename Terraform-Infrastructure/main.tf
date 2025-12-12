d42c9a844c0edebfb7e5ffeca1c6a5bd55605e26
Beyond003 <abdelrahmaneeennn@example.com>
2025-12-13 01:35:01 +0200

    Add Ansible-for creating nexus file, and Terraform Infra

---
+++ Terraform-Infrastructure/main.tf
@@ -0,0 +1,124 @@
+# main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 1
+      max_size     = 1
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+# variables.tf
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "demo-vpc"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "demo-eks"
+}
+
+variable "cluster_version" {
+  type    = string
+  default = "1.29"
+}
+
+# EC2 instance in public subnet
+# I want to specifies the security group
+resource "aws_instance" "public_ec2" {
+  ami                         = "ami-0c94855ba95c71c99" # Amazon Linux 2 AMI example
+  instance_type               = "t2.large"
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Anisble-Nexus-Server"
+  }
+}
+
+# outputs.tf
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
